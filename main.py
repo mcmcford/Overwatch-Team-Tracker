c3d86808d103fad091752d754b3731ceb9705418
Morgan McFord (mcfordm) <mcmcford93@gmail.com>
2022-01-12 16:03:50 +0000

    added basic command and guild_ids into config

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import configparser
+import interactions
 import os
 
 config = configparser.ConfigParser()
 
 if os.path.exists('config.ini') == False:
-    config['DEFAULT'] = {'bot_token': '123xyz'}
+    config['DEFAULT'] = {'bot_token': '123xyz','guild_ids': '123456789101234567,123456789012345678'}
     config['DATABASE'] = {'db_username': 'defaultusername','db_password': 'defaultpassword','db_ip': 'dbip','db_port': '3306'}
     config['RESOURCES'] = {'tesseract_path': 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'}
     with open('config.ini', 'w') as configfile:
@@ -26,3 +27,15 @@ database_username = config['DATABASE']['db_username']
 database_password = config['DATABASE']['db_password']
 database_ip = config['DATABASE']['db_ip']
 database_port = config['DATABASE']['db_port']
+
+bot = interactions.Client(token=bot_token)
+
+guild_ids = config['DEFAULT']['guild_ids'].split(',')
+
+@bot.command(
+    name='analyse',
+    description='Analyse a given image',
+    scope=guild_ids
+)
+async def analyse(ctx, *args):
+    print("here!")
