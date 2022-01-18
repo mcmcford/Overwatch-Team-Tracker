685eb9401306ea8a3ad0dbccd870e1f9773d9e1b
Morgan McFord (mcfordm) <mcmcford93@gmail.com>
2022-01-18 21:36:28 +0000

    added search function

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import requests
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import urllib.request
 from PIL import Image
 import configparser
 import discord
+import random as rand
 from discord.ext import commands
 from discord.ext.commands import Bot
 from discord.ext.commands import has_permissions, MissingPermissions
@@ -12,6 +18,7 @@ from discord_components import *
 import pytesseract
 import cv2
 import os
+import time
 
 config = configparser.ConfigParser()
 
@@ -88,6 +95,8 @@ async def analyse(ctx):
 
     im.save("temp_1.png")
 
+    users = []
+
 
     for x in range(2):
         top = 300
@@ -97,6 +106,11 @@ async def analyse(ctx):
 
             left = 740
             right = 1102
+
+            pfp_left = 670
+            pfp_right = 740
+            pfp_bottom = bottom + 42
+            pfp_top = top - 6
 
             red_lower_limit_one = 83
             red_upper_limit_one = 91
@@ -119,6 +133,11 @@ async def analyse(ctx):
         else:
             left = 1290
             right = 1652
+            
+            pfp_left = 1220
+            pfp_right = 1290
+            pfp_bottom = bottom + 42
+            pfp_top = top - 6
 
             red_lower_limit_one = 155
             red_upper_limit_one = 170
@@ -133,7 +152,13 @@ async def analyse(ctx):
         for x in range(6):
 
             im1 = im.crop((left, top, right, bottom))
-            im1.save("name.png")
+
+            # generate a random number between 0 and 9999999999, padding the left with zeros to keep them all the same length
+            random_number = str(rand.randint(0, 9999999999)).zfill(10)
+            name_image = f"name-{random_number}.png"
+
+
+            im1.save(name_image)
 
             rgb_im = im1.convert('RGB')
             r, g, b = rgb_im.getpixel((1, 1))
@@ -151,21 +176,139 @@ async def analyse(ctx):
 
             if ((r >= red_lower_limit_one and r <= red_upper_limit_one) or  (r >= red_lower_limit_two and r <= red_upper_limit_two)) and ((g >= green_lower_limit_one and g <= green_upper_limit_one) or (g >= green_lower_limit_two and g <= green_upper_limit_two)) and ((b >= blue_lower_limit_one and b <= blue_upper_limit_one) or (b >= blue_lower_limit_two and b <= blue_upper_limit_two)):
 
-                img = cv2.imread("name.png")
+
+                pfp_image = f"pfp-{random_number}.png"
+
+                img = cv2.imread(name_image)
                 text = pytesseract.image_to_string(img)
 
-                # remove anything after any spaces (such as the users full name if they're friends)
-                text = (text.split())[0]
+                im2 = im.crop((pfp_left, pfp_top, pfp_right, pfp_bottom))
+                im2.save(pfp_image)
 
-                await ctx.send(text,file=discord.File('name.png'))
+                # remove anything after any spaces (such as the users full name if they're friends)
+                try:
+                    text = (text.split())[0]
+                except:
+                    text = text
+
+                users.append([text, random_number])
+
+                #await ctx.send(text,file=discord.File(name_image))
+                #await ctx.send(file=discord.File(pfp_image))
             else:
-                await ctx.send("Failed RGB test:\nRGB = " + str(r) + " " +  str(g) + " " + str(b),file=discord.File('name.png'))
+                await ctx.send("Failed RGB test:\nRGB = " + str(r) + " " +  str(g) + " " + str(b),file=discord.File(name_image))
 
             top += 80
             bottom += 80
+            pfp_top += 80
+            pfp_bottom += 80
 
 
+    for x in users:
+        print(x)  
     
+    option = webdriver.ChromeOptions()
+    # I use the following options as my machine is a window subsystem linux. 
+    # I recommend to use the headless option at least, out of the 3
+    option.add_argument('--headless')
+    option.add_argument('--no-sandbox')
+    option.add_argument('--disable-dev-sh-usage')
+
+    driver = webdriver.Chrome('C:\\Users\\morga\\Documents\\GitHub\\Overwatch-Team-Tracker\\chromedriver.exe', options=option)
+
+    for x in range(len(users)):
+        
+        name = users[x][0]
+
+        driver.get("https://www.overbuff.com/search?q=" + name) # Getting page HTML through request
+        
+        print(str(x) + " in " + str(len(users)))
+        print(users)
+        
+        not_found = False
+        multi_users = []
+        
+        # wait for page to load players
+        try:
+            elem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'SearchResult')))
+            time.sleep(10)
+        except selenium.common.exceptions.TimeoutException:
+            not_found = True
+        finally:
+            soup = BeautifulSoup(driver.page_source, 'html.parser') # Parsing content using beautifulsoup. Notice driver.page_source instead of page.content
+        
+        if not_found == False:
+            #print(soup)
+
+            title = soup.title.text
+            links = soup.find_all('a')
+
+            links_split = str(links).split("<a class=\"SearchResult\"")
+            links_split = links_split[1:] # remove first index in the array
+            
+            if len(links_split) > 1:
+                multi_users.append([])
+            
+            for y in range(len(links_split)):
+                
+                if y == len(links_split) -1:
+                    
+                    temp = links_split[y].split(", <a class=\"\" href=\"/about\">About</a>, ")
+                    links_split[y] = temp[0]
+                
+                # get URL
+                temp = links_split[y].split("href=\"")
+                end = (temp[1].split("\">"))[0].replace(" ", "%20")
+                url = "https://www.overbuff.com" + end
+                
+                # get icon
+                temp = links_split[y].split("<img class=\"image-player image-icon\" src=\"")
+                icon = (temp[1].split("\"/>"))[0]
+                
+                if "cloudfront" in icon:
+                    print("")
+                else:
+                    icon = "https://www.overbuff.com" + icon
+                
+                # get level
+                temp = links_split[y].split("player-level\">")
+                level = (temp[1].split("</div>"))[0]
+                
+                try:
+                    # get blizzard ID
+                    temp = url.split("/")
+                    temp = temp[-1].split("-")
+                    name = temp[0] + "#" + temp[1].replace("%20", " ")
+                except:
+                    name = (url.split("/"))[-1]
+                
+                print("URL: " + url)
+                print("icon: " + icon)
+                print("level: " + level)
+                print("name: " + name)
+                
+                if len(links_split) > 1:
+                    multi_users[len(multi_users)-1].append([])
+                    multi_users[len(multi_users)-1][y].append(url)
+                    multi_users[len(multi_users)-1][y].append(icon)
+                    multi_users[len(multi_users)-1][y].append(level)
+                    multi_users[len(multi_users)-1][y].append(name)
+                    
+                else:
+
+                    # send an embed
+
+                    embed = discord.Embed(title=name, description="View this player [here](" + url +")")
+                    embed.add_field(name='Level', value=level)
+                    embed.set_thumbnail(url=icon)
+                    embed.set_footer(text="Provided by Morgan")
+                    await ctx.send(embed=embed)
+                
+                print("\n\n")
+            
+            if len(multi_users) > 0:
+                for y in multi_users:
+                    print(y)
 
 
 bot.run(bot_token)
